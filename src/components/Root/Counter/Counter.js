18b55ee622d62cb314f7286cf6f737e9299a22c9
siva921 <60959198+siva921@users.noreply.github.com>
2020-03-06 10:37:33 +0530

    Update Counter.js

--- src/components/Root/Counter/Counter.js
+++ src/components/Root/Counter/Counter.js
@@ -41,7 +41,7 @@ const Counter = () => {
 
   return (
     <div>
-      <CounterValue data-testid="counter-value">Count - testing1 -demo : {counter}</CounterValue>
+      <CounterValue data-testid="counter-value">Count - testing2 -demo : {counter}</CounterValue>
       <ButtonRow>
         <Button onClick={() => dispatch(-2)}>-2</Button>
         <Button onClick={() => dispatch(-1)}>-1</Button>
